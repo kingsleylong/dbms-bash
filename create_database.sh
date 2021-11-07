5aad33068d778bde88a3b37e4957444ef8f7f58b
Kingsley <long.kinglsey@gmail.com>
2021-11-07 12:25:03 +0000

    parameter checking

--- create_database.sh
+++ create_database.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# Check parameters
+if [ $# -eq 0 ]; then
+	echo "Error: no paramter"
+	exit 1
+fi
+
+database="$1"
+if [ -e "$database" ]; then
+	echo "Error: DB${database} already exists"
+fi
