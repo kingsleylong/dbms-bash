8ecab759c5d8e1f9a343a67984b4d25caf1a74e5
Yu Long <yu.long@ucdconnect.ie>
2021-12-12 19:09:18 +0000

    implemented select by column names

--- select.sh
+++ select.sh
@@ -32,22 +32,26 @@ fi
 # Check if columns are provided, if empty then replace with all columns (the fields used in cut command)
 columns="$3"
 schema=$(head -n 1 "$database/$table")
+select_column_idx=()
 if [ -z "$columns" ]; then
-	columns="1-"
-else
+	columns="${schema}"
+fi
 	# split the sepecified column names to an array
 	select_columns=($(echo "$columns" | cut -d',' -f1- --output-delimiter=" "))
-	echo "[DEBUG]select columns: ${select_columns[@]}, size:${#select_columns[@]}"
+	#echo "[DEBUG]select columns: ${select_columns[@]}, size:${#select_columns[@]}"
 	schema_columns=($(echo "$schema" | cut -d',' -f1- --output-delimiter=" "))
-	echo "[DEBUG]schema columns: ${schema_columns[@]}, size:${#schema_columns[@]}"
+	#echo "[DEBUG]schema columns: ${schema_columns[@]}, size:${#schema_columns[@]}"
 	# for each column name in the array, test if it exists in the schema
 	for col in ${select_columns[@]}; do
 		valid=false
-		for scol in ${schema_columns[@]}; do
-			echo "[DEBUG]col=$col, scol=$scol"
+		for (( i=0; i<${#schema_columns[@]}; i++ )); do
+		#for scol in ${schema_columns[@]}; do
+			scol=${schema_columns[$i]}
+			#echo "[DEBUG]col=$col, scol=$scol"
 			if [ "${col}" = "${scol}" ]; then
-				echo "[DEBUG]equal: col=$col, scol=$scol"
+			#	echo "[DEBUG]equal: col=$col, scol=$scol"
 				valid=true
+				select_column_idx+=($i)
 				break
 			fi
 		done
@@ -57,10 +61,21 @@ else
 			exit 4
 		fi
 	done
-fi
 
+#echo "[DEBUG]select_column_idx: ${select_column_idx[@]}"
 echo 'start_result'
-cut -d',' -f"$columns" "$database/$table"
+while read row; do
+	#echo "[DEBUG]row: $row"
+	select_col_cnt=${#select_column_idx[@]}
+	for (( i=0; i<${select_col_cnt}; i++ )); do
+		echo -n $row | cut -d',' -f"$(( ${select_column_idx[$i]} + 1 ))" -z
+		if [ $i -lt $(( $select_col_cnt - 1 )) ]; then
+			echo -n ","
+		else
+			echo ""
+		fi
+	done
+done < "$database/$table"
 echo 'end_result'
 ./V.sh "$database"
 exit 0
